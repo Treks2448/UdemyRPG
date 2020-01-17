15047dd6848181619bf4df1ba7bf0eb89625c1f3
Igor <igorgras100@gmail.com>
2020-01-17 14:15:48 +0000

    Implemented click to move controls for player and camera following.

---
+++ UdemyRPG/Assets/Scripts/Mover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    // Variables visible in editor
+    [SerializeField]
+    GameObject target;
+
+    // Private member variables
+    Camera cam;
+    Ray ray;
+    RaycastHit hit;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit)) { GetComponent<NavMeshAgent>().destination = hit.point; }
+        }
+    }
+}
